9d0c0851e04f0abd21e8d379992af72d9933e88d
Aman <aman16singh2001@gmail.com>
2023-02-02 00:34:27 +0530

    setting up the view engine ejs

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const port=8000;
 app.use('/',require('./routes'));
 
 
+//setting up the view engine
+app.set('view engine','ejs');
+app.set('views','./views');
+
 app.listen(port , function(err){
 if(err){
     console.log(`error in running the server : ${port}`);
