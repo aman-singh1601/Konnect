6dc89841fc8a3dc62f70568df1819219046154e2
Aman <aman16singh2001@gmail.com>
2023-04-23 16:01:59 +0530

    converted the code into async await

--- controllers/home_controllers.js
+++ controllers/home_controllers.js
@@ -1,25 +1,46 @@
 const User=require('../models/user');
 const Post=require('../models/post');
 
-module.exports.home=function(req,res){ 
-    Post.find({})
-    .populate('user')
-    .populate({
-        path:'comments',
-        populate:{
-            path:'user'
-        }
-    })
-    .exec(function(err,posts){
-        User.find({},function(err,users){
-            return res.render('home',{
-                title: " Home",
-                posts:posts,
-                all_users:users
-            });
-        })
+module.exports.home= async function(req,res){ 
+
+    try{
+        let posts= await Post.find({}).populate('user').populate({
+            path:'comments',
+            populate:{
+                path:'user'
+            }
+        });
+    
+        let users= await User.find({});
+    
+        return res.render(
+                        'home',{
+                         title: " Home",
+                         posts:posts,
+                         all_users:users
+         });
+    }catch(err){
+        console.log("error",err);
+        return ;
+    }
+    // Post.find({})
+    // .populate('user')
+    // .populate({
+    //     path:'comments',
+    //     populate:{
+    //         path:'user'
+    //     }
+    // })
+    // .exec(function(err,posts){
+    //     User.find({},function(err,users){
+    //         return res.render('home',{
+    //             title: " Home",
+    //             posts:posts,
+    //             all_users:users
+    //         });
+    //     })
        
-    })
+    // })
 
 }
 
--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,28 +1,37 @@
 const Post=require('../models/post');
 const Comment=require('../models/comment');
 
-module.exports.create=function(req,res){
-    Post.create({
-        content:req.body.content,
-        user:req.user._id
-    },function(err,post){
-        if(err){
-            console.log('error in creating a post');
-            return ;
-        }
-        return res.redirect('back');
-    })
-}
-
-module.exports.destroy=function(req,res){
- Post.findById(req.params.id,function(err,post){
-
-    if(post.user==req.user.id){
-        post.remove();
-        Comment.deleteMany({post:req.user.id},function(err){
-            return res.redirect('back');
+module.exports.create= async function(req,res){
+    try{
+        await Post.create({
+            content:req.body.content,
+            user:req.user._id
         })
-    }else return res.redirect('back');
- })
+    
+        return res.redirect('back');
+    }
+    catch(err){
+        console.log('Error ',err);
+        return;
+    }
+    
+    }
+    
+
+
+module.exports.destroy= async function(req,res){
+    try{
+        let post=await Post.findById(req.params.id);
+        if(post.user==req.user.id){
+           post.remove();
+           await Comment.deleteMany({post:req.user.id});
+           return res.redirect('back');
+        } else
+           return res.redirect('back');
+    }catch(err){
+        console.log('Error ',err);
+        return;
+    }
+   
 
 }
